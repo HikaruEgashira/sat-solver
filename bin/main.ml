b64e420c1a237ce88ab05ffb9fe9c827b287f4d1
Hikaru Egashira <shighten@gmail.com>
2022-01-09 23:04:57 +0900

    feat ファイルの内容を出力する

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,18 @@
-let () = print_endline "Hello, World!"
+let file = "./cnf/lesson1.dimacs" (* (P1∨P2)∧(￢P1∨P2)∧(￢P1∨￢P2) *)
+
+let rec print_file channel =
+  match input_line channel with
+  | line -> print_endline line; print_file channel
+  | exception End_of_file -> () 
+
+let () =
+  let ic = open_in file in
+  try 
+    print_file ic;               (* print the file *)
+    close_in ic                  (* 入力チャネルを閉じる *)
+  with e ->                      (* 期待しない例外が起こったとき *)
+    close_in_noerr ic;           (* 緊急にチャネルを閉じる *)
+    raise e                      (* エラー終了: ファイルは閉じられるが
+                                    チャネルはフラッシュされない *)
+
+  (* 通常終了: チャネルは全てフラッシュされて閉じられる *)
