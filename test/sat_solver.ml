92cab8709af48f2f47472e78fa4924290b896065
Hikaru Egashira <shighten@gmail.com>
2022-01-28 09:49:03 +0900

    wip test

--- test/sat_solver.ml
+++ test/sat_solver.ml
@@ -1,1 +1,17 @@
-let () = print_endline "Hello, Test!"
+open Sat_solver.Solver
+open Sat_solver.Parser
+open Sat_solver.Printer
+
+let input_file = ref "../cnf/lesson1.dimacs"
+
+let () =
+  (* ファイルからCNFを生成 *)
+  let channel = open_in input_file.contents in
+  let cnf = parse_cnf channel in
+  close_in channel;
+
+  (* solve *)
+  let ret = solve cnf in
+
+  (* 出力 *)
+  print_ret ret
